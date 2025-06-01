633460ce7f80253e8cd79d9ff689b4018372c1dc
thaivhm <hoangminhthaiv@gmail.com>
2025-06-01 15:32:33 +0700

    fix restructure errors

--- user-ui/src/App.tsx
+++ user-ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import RegisterPage from "./pages/auth/register";
 
 const queryClient = new QueryClient();
 
@@ -8,7 +9,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <div className="flex justify-center">
-          <div className="flex justify-center w-4/5 mt-5"></div>
+          <div className="flex justify-center w-4/5 mt-5">
+            <RegisterPage />
+          </div>
         </div>
       </BrowserRouter>
     </QueryClientProvider>
