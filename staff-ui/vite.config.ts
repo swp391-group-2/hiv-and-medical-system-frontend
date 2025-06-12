548f94f2a354692148943e3c3072e8b4b4b2863a
thaivhm <hoangminhthaiv@gmail.com>
2025-06-12 14:09:24 +0700

    modified: server config

--- staff-ui/vite.config.ts
+++ staff-ui/vite.config.ts
@@ -5,6 +5,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080/hiv/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
