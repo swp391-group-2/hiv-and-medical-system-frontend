d972e9faa828175e67b9c34d4cb76ebd32672b10
thaivhm <hoangminhthaiv@gmail.com>
2025-06-15 00:29:57 +0700

    create: lab ui

---
+++ staff-ui/src/components/lab/lab-table.tsx
@@ -0,0 +1,37 @@
+import {
+  Table,
+  TableHeader,
+  TableHead,
+  TableBody,
+  TableRow,
+} from "@/components/ui/table";
+import type { Appointment } from "@/types/types";
+import { EmptyListMessage } from "@/components/page-message";
+import { LabRow } from "./lab-row";
+
+export function LabTable({ data }: { data: Appointment[] }) {
+  if (data.length == 0) {
+    return <EmptyListMessage message="Không có lịch hẹn nào đang chờ" />;
+  }
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Bệnh nhân</TableHead>
+          <TableHead>Loại khám</TableHead>
+          <TableHead>Ngày</TableHead>
+          <TableHead>Giờ</TableHead>
+          <TableHead>Bác sĩ</TableHead>
+          <TableHead>Trạng thái</TableHead>
+          <TableHead>Thao tác</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((appt) => (
+          <LabRow key={appt.appointmentId} appt={appt} />
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
---
+++ staff-ui/src/pages/lab/lab.tsx
@@ -0,0 +1,50 @@
+import { useAppointments } from "@/api/appointments";
+import { AppointmentFilters } from "@/components/appointments/appointment-filters";
+import { LabTable } from "@/components/lab/lab-table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { formatDMY } from "@/lib/utils";
+import { useMemo } from "react";
+
+const Lab = () => {
+  const {
+    data: appointments = [],
+    isLoading,
+    isError,
+    error,
+  } = useAppointments();
+
+  const filtered = useMemo(
+    () =>
+      Array.isArray(appointments)
+        ? appointments.filter((a) => a.status === "CHECKED_IN")
+        : [],
+    [appointments]
+  );
+  if (isLoading) return <div>Loading…</div>;
+  if (isError)
+    return <div className="text-red-600">{(error as Error).message}</div>;
+  return (
+    <section className="w-full mt-7">
+      <div>
+        <h1 className="text-3xl font-bold mb-5">Danh Sách Chờ Xét Nghiệm</h1>
+        <p className="text-gray-500">
+          Hôm nay là: {formatDMY(new Date().toISOString())}
+        </p>
+      </div>
+      <AppointmentFilters onApply={(f) => f.search} />
+      <Tabs defaultValue="list">
+        <TabsList>
+          <TabsTrigger value="list">Danh sách</TabsTrigger>
+          <TabsTrigger value="calendar" disabled>
+            Lịch
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="list">
+          <LabTable data={filtered} />
+        </TabsContent>
+      </Tabs>
+    </section>
+  );
+};
+
+export default Lab;
