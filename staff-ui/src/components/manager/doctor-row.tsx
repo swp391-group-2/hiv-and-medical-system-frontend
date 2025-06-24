5337bf5dce2842a82b0686981a44a4f24bc9a601
thaivhm <hoangminhthaiv@gmail.com>
2025-06-24 23:00:46 +0700

    add: hover card when mouse over status icon

--- staff-ui/src/components/manager/doctor-row.tsx
+++ staff-ui/src/components/manager/doctor-row.tsx
@@ -2,6 +2,12 @@ import { TableRow, TableCell } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Calendar, Edit, Trash2, Dot } from "lucide-react";
 import type { Doctor } from "@/types/doctor";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
 export function DoctorRow({
   doctor,
   handleDeleteDoctor,
@@ -17,21 +23,32 @@ export function DoctorRow({
       <TableCell>{doctor.specialization}</TableCell>
       <TableCell>{doctor.licenseNumber}</TableCell>
       <TableCell>
-        {doctor.userStatus === "active" ? (
-          <Dot
-            className="cursor-pointer ml-5"
-            size={16}
-            strokeWidth={16}
-            color="#34D399"
-          />
-        ) : (
-          <Dot
-            className="cursor-pointer ml-5"
-            size={16}
-            strokeWidth={16}
-            color="#EF4444"
-          />
-        )}
+        <HoverCard openDelay={200} closeDelay={200}>
+          <HoverCardTrigger>
+            {doctor.userStatus === "active" ? (
+              <Dot
+                className="cursor-pointer ml-5"
+                size={16}
+                strokeWidth={16}
+                color="#34D399"
+              />
+            ) : (
+              <Dot
+                className="cursor-pointer ml-5"
+                size={16}
+                strokeWidth={16}
+                color="#EF4444"
+              />
+            )}
+          </HoverCardTrigger>
+          <HoverCardContent>
+            <div>
+              {doctor.userStatus === "active"
+                ? "Tài khoản đang hoạt động"
+                : "Tài khoản bị vô hiệu hoá"}
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       </TableCell>
       <TableCell className="flex items-center gap-2">
         <Button variant="outline" size="sm">
