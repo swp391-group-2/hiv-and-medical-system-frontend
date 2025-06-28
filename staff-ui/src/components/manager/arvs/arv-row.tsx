c95d25bbe7bc90abb8c5c46ab46a60d9ad65e558
thaivhm <hoangminhthaiv@gmail.com>
2025-06-28 21:59:22 +0700

    add: update arv form

--- staff-ui/src/components/manager/arvs/arv-row.tsx
+++ staff-ui/src/components/manager/arvs/arv-row.tsx
@@ -7,6 +7,14 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import type { Prescription } from "@/types/types";
+import {
+  Dialog,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogContent,
+} from "@/components/ui/dialog";
+import { UpdateArvForm } from "./update-form";
 
 export function ArvRow({ arv }: { arv: Prescription }) {
   return (
@@ -15,20 +23,33 @@ export function ArvRow({ arv }: { arv: Prescription }) {
       <TableCell>{arv.name}</TableCell>
 
       <TableCell className="grid grid-cols-2 items-center gap-4">
-        <Tooltip delayDuration={500}>
-          <TooltipTrigger>
-            <Button
-              className="cursor-pointer w-full"
-              variant="outline"
-              size="sm"
-            >
-              <Edit className="h-4 w-4" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Chỉnh sửa thông tin</p>
-          </TooltipContent>
-        </Tooltip>
+        <Dialog>
+          <Tooltip delayDuration={500}>
+            <TooltipTrigger>
+              <DialogTrigger asChild>
+                <Button
+                  className="cursor-pointer w-full"
+                  variant="outline"
+                  size="sm"
+                >
+                  <Edit className="h-4 w-4" />
+                </Button>
+              </DialogTrigger>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Chỉnh sửa thông tin</p>
+            </TooltipContent>
+          </Tooltip>
+          <DialogContent className="flex flex-col justify-between min-w-[1100px] min-h-[800px]">
+            <DialogHeader>
+              <DialogTitle>Cập nhật phác đồ</DialogTitle>
+            </DialogHeader>
+            <UpdateArvForm
+              className="flex flex-col justify-between space-y-4 grow"
+              arv={arv}
+            />
+          </DialogContent>
+        </Dialog>
       </TableCell>
     </TableRow>
   );
