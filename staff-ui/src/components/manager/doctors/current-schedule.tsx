2b7ff11bcfad8cfdd28807be229b8004123aae26
thaivhm <hoangminhthaiv@gmail.com>
2025-06-28 16:59:23 +0700

    add: get current week schedule by doctor's id

--- staff-ui/src/components/manager/doctors/current-schedule.tsx
+++ staff-ui/src/components/manager/doctors/current-schedule.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 export const CurrentSchedule = () => {
   return (
     <div className="flex flex-col gap-3 items-end w-full h-full">
-      <div className="w-full h-full border border-gray-100 rounded">
-        <div></div>
-        <div></div>
+      <div className="flex w-full h-full border shadow border-gray-100 rounded">
+        <div className="w-1/12 border border-gray-300 "></div>
+        <div className="w-full grid grid-cols-7 gap-2 border border-gray-300"></div>
       </div>
       <div>
         <Button>Sửa</Button>
