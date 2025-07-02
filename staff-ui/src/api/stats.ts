07edfd1b828c99384f44cb65d054f4c441960e16
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2025-07-02 23:55:13 +0700

    done manager stats

--- staff-ui/src/api/stats.ts
+++ staff-ui/src/api/stats.ts
@@ -2,29 +2,31 @@ import type {
   ServiceTypeStat,
   StatCardStaticProps,
   FavoriteDoctor,
+  WeeklyStatsData,
 } from "@/types/stats";
 import type { Response } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
 import http from "./http";
 
 export const getFeaturedStats = async (
-  milestone: string
+  startDate: string,
+  endDate: string
 ): Promise<StatCardStaticProps[]> => {
   const { data } = await http.get<Response<StatCardStaticProps>>(
     `/dashboards/stats`,
     {
-      params: { milestone },
+      params: { startDate, endDate },
     }
   );
   return data.data;
 };
 
-export function useFeaturedStats(milestone: string) {
+export function useFeaturedStats(startDate: string, endDate: string) {
   return useQuery<StatCardStaticProps[]>({
-    queryKey: ["featured-stats", milestone],
+    queryKey: ["featured-stats", startDate, endDate],
     queryFn: ({ queryKey }) => {
-      const [, milestone] = queryKey as [string, string];
-      return getFeaturedStats(milestone);
+      const [, startDate, endDate] = queryKey as [string, string, string];
+      return getFeaturedStats(startDate, endDate);
     },
     staleTime: Infinity,
   });
@@ -62,6 +64,30 @@ export const useFavoriteDoctors = (page?: number, size?: number) => {
   return useQuery<FavoriteDoctor[]>({
     queryKey: ["topDoctors", page, size],
     queryFn: () => getFavoriteDoctors(page, size),
+    staleTime: Infinity,
+  });
+};
+
+export const getWeeklyStats = async (
+  startDate: string,
+  endDate: string
+): Promise<WeeklyStatsData[]> => {
+  const { data } = await http.get<Response<WeeklyStatsData>>(
+    `/dashboards/stats/weekly`,
+    {
+      params: { startDate, endDate },
+    }
+  );
+  return data.data;
+};
+
+export const useWeeklyStats = (startDate: string, endDate: string) => {
+  return useQuery<WeeklyStatsData[]>({
+    queryKey: ["weekly-stats", startDate, endDate],
+    queryFn: ({ queryKey }) => {
+      const [, startDate, endDate] = queryKey as [string, string, string];
+      return getWeeklyStats(startDate, endDate);
+    },
     staleTime: Infinity,
   });
 };
