01863eb30e3f60e731149c788e2f8511ff66f572
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2025-07-12 12:45:00 +0700

    fix modal

--- doctor-ui/src/components/Modal/basicModal.tsx
+++ doctor-ui/src/components/Modal/basicModal.tsx
@@ -11,7 +11,7 @@ const BasicModal: React.FC<ModalProps> = ({ open, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-black/40 z-50 flex items-center justify-center">
-      <div className="relative bg-white rounded-xl shadow-lg p-6 w-full max-w-3xl max-h-[90vh] overflow-y-auto">
+      <div className="relative bg-white rounded-xl shadow-lg p-6 w-full max-w-6xl max-h-[90vh] overflow-y-auto">
         {/* Nút đóng */}
         <button
           onClick={onClose}
